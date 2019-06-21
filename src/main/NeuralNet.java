72ee9ec9115357d1edbbf8005c92bb29e658cdb9
Benjamin Xu <thequantumpotato@gmail.com>
2019-06-21 14:44:46 +0800

    added framework for neural network

---
+++ src/main/NeuralNet.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.util.ArrayList;
+
+/**
+ *  Basic implementation of neural network
+ *  @author Benjamin Xu
+ */
+public class NeuralNet {
+
+    private InputLayer inputLayer;
+    private HiddenLayer hiddenLayer;
+    private ArrayList<HiddenLayer> listOfHiddenLayer;
+    private OutputLayer outputLayer;
+    private int numberOfHiddenLayers;
+
+    /** Initializes the neural network */
+    public void initNet(){
+
+    }
+
+    /** Prints input and output weights for each layer */
+    public void printNet(){
+
+    }
+
+}
