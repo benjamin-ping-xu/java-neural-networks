df51bbc2f612941ca72ea79dc2bf39fc3921f24e
Benjamin Xu <thequantumpotato@gmail.com>
2019-06-21 14:29:56 +0800

    added framework for input layer

---
+++ src/main/InputLayer.java
@@ -0,0 +1,25 @@
+package main;
+
+/**
+ *  Implementation of input layer to neural network
+ *  @author Benjamin Xu
+ */
+public class InputLayer extends Layer{
+
+    /**
+     *  Initializes the input layer with pseudo-random real numbers
+     *  @param inputLayer the input layer to initialize
+     */
+    public void initLayer(InputLayer inputLayer){
+
+    }
+
+    /**
+     *  Prints the input weights of the layer
+     *  @param inputLayer the layer to print input weights for
+     */
+    public void printLayer(InputLayer inputLayer){
+
+    }
+
+}
