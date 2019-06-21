2f524030f7c95f6669ac5c9b18bf1968cf1debdb
Benjamin Xu <thequantumpotato@gmail.com>
2019-06-21 14:39:20 +0800

    added framework for output layer

--- src/main/OutputLayer.java
+++ src/main/OutputLayer.java
@@ -6,4 +6,21 @@ package main;
  */
 public class OutputLayer extends Layer{
 
+    /**
+     *  Initializes an output layer with pseudo-random real numbers
+     *  @param outputLayer output layer to initialize
+     *  @return output layer
+     */
+    public OutputLayer initLayer(OutputLayer outputLayer){
+        return new OutputLayer();
+    }
+
+    /**
+     *  Prints the weights of the output layer
+     *  @param outputLayer the output layer to print weights for
+     */
+    public void printLayer(OutputLayer outputLayer){
+
+    }
+
 }
