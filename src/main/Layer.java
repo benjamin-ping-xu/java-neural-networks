011f1b1af0cc79bdedf82893afa911f2c78f644d
Benjamin Xu <thequantumpotato@gmail.com>
2019-06-21 14:20:19 +0800

    added framework for layer

--- src/main/Layer.java
+++ src/main/Layer.java
@@ -1,8 +1,48 @@
 package main;
 
+import java.util.ArrayList;
+
 /**
  *  Abstract definition for a neural network layer
  *  @author Benjamin Xu
  */
-public class Layer {
+public abstract class Layer {
+
+    private ArrayList<Neuron> listOfNeurons;
+    private int numberOfNeuronsInLayer;
+
+    public Layer(){
+    }
+
+    /**
+     *  Return the list of neurons in the layer
+     *  @return a list of neurons
+     */
+    public ArrayList<Neuron> getListOfNeurons() {
+        return listOfNeurons;
+    }
+
+    /**
+     *  Set the list of neurons in the layer
+     *  @param listOfNeurons the list of neurons to set for the layer
+     */
+    public void setListOfNeurons(ArrayList<Neuron> listOfNeurons) {
+        this.listOfNeurons = listOfNeurons;
+    }
+
+    /**
+     *  Returns the number of neurons in the layer
+     *  @return the number of neurons
+     */
+    public int getNumberOfNeuronsInLayer() {
+        return numberOfNeuronsInLayer;
+    }
+
+    /**
+     *  Set the number of neurons in the layer
+     *  @param numberOfNeuronsInLayer the number of neurons to set for the layer
+     */
+    public void setNumberOfNeuronsInLayer(int numberOfNeuronsInLayer) {
+        this.numberOfNeuronsInLayer = numberOfNeuronsInLayer;
+    }
 }
